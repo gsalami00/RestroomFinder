3c114091be37ce971231617ef90638abaf9a2103
Geraldine Salamat <g.a.salamat@gmail.com>
2018-07-26 08:25:53 -0400

    add restroom form

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SignUp from './Components/Forms/SignUp'
+import AddRestroom from './Components/Forms/AddRestroom'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <SignUp />
-
+      {/* <SignUp /> */}
+      <AddRestroom />
       </View>
     );
   }
